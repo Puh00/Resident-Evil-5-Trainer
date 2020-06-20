a1f7fe36d008f8730c024115910a9319844925c1
Puh00 <alex.phu@live.com>
2020-06-21 00:00:13 +0200

    Form now not resizable and removed maximize window function

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -402,7 +402,9 @@
             this.Controls.Add(this.ammoCheckBox);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Resident Evil 5 V1.0.0.129 Trainer by @Puh00";
             this.Load += new System.EventHandler(this.Form1_Load);
